5fbccc689f133d416708fb2c8b7653237e5237a1
eakkasit <eakkasit.p@avlgb.com>
2022-12-27 19:10:23 +0700

    etc

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -12,6 +12,7 @@ export const getStorage = (key) => {
 export const getLineProfile = () => {
   const obj = {
     _id: getStorage("_id"),
+    line_id: getStorage("line_id"),
     name: getStorage("line_name"),
     img: getStorage("line_img"),
     line_status: getStorage("line_status"),
